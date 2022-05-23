4bea5776b2bafe9e0588574ade4976f344307f06
pccyh <1064187512@qq.com>
2022-05-23 20:55:29 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("123123");
+        System.out.println("123123");
+        System.out.println("123123");
+        System.out.println("123123");
+        System.out.println("123123");
+    }
+}
